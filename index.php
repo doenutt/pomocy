70c43b8ee9660ae9b11e320a924b963458a2c80a
doenutt <paula20209@hotmail.com>
2015-06-16 23:05:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>PHP Form</title>
+		<meta charset="utf-8" />
+		<meta name="Author" content="Dupek" />
+
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+	</head>
+	<body>
+		<div id="content">
+			<h4 id="header">Wypełnij pola:</h4>
+			<form action="filled.php" method="POST">
+				<div class="element">
+					<label for="imie">Imie: </label>
+					<input type="text" id="imie" name="imie" />
+				</div>
+				<div class="element">
+					<label for="wiek">Wiek: </label>
+					<input type="number" id="wiek" name="wiek" />
+				</div>
+				<div class="element">
+					<label for="zainteresowania">Twoje hobby: </label>
+			<select name="zainteresowania" required="required">
+			<option label="pilkanozna">piłka nożna
+			<option label="siatkowka">siatkówka
+			<option label="tenis">tenis
+			</select>
+				</div>
+				<input type="submit" value="Wyślij" />
+ 			</form>
+		</div>
+	</body>
+</html>
